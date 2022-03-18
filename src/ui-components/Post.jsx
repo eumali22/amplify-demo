712b87b7497e893edadf9b3d79c382a407dd1b97
eumali22 <97612829+eumali22@users.noreply.github.com>
2022-03-18 20:21:53 +0800

    UI components from figma

---
+++ src/ui-components/Post.jsx
@@ -0,0 +1,79 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Flex, Text, View } from "@aws-amplify/ui-react";
+export default function Post(props) {
+  const { overrides: overridesProp, ...rest } = props;
+  const overrides = { ...overridesProp };
+  return (
+    <Flex
+      gap="10px"
+      direction="column"
+      width="509px"
+      height="213px"
+      overflow="hidden"
+      position="relative"
+      padding="45px 33px 45px 33px"
+      backgroundColor="rgba(255,255,255,1)"
+      {...rest}
+      {...getOverrideProps(overrides, "Flex")}
+    >
+      <Text
+        fontFamily="Inter"
+        fontSize="18px"
+        fontWeight="400"
+        color="rgba(0,0,0,1)"
+        lineHeight="24px"
+        textAlign="left"
+        display="flex"
+        direction="column"
+        justifyContent="flex-start"
+        height="26px"
+        shrink="0"
+        alignSelf="stretch"
+        objectFit="cover"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        whiteSpace="pre-wrap"
+        children="Post Title"
+        {...getOverrideProps(overrides, "Flex.Text[0]")}
+      ></Text>
+      <View
+        height="87px"
+        grow="1"
+        basis="87px"
+        alignSelf="stretch"
+        objectFit="cover"
+        overflow="hidden"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "Flex.View[0]")}
+      >
+        <Text
+          fontFamily="Inter"
+          fontSize="12px"
+          fontWeight="400"
+          color="rgba(0,0,0,1)"
+          lineHeight="24px"
+          textAlign="left"
+          display="flex"
+          direction="column"
+          justifyContent="flex-start"
+          position="absolute"
+          top="0px"
+          left="0px"
+          padding="0px 0px 0px 0px"
+          whiteSpace="pre-wrap"
+          children="Content"
+          {...getOverrideProps(overrides, "Flex.View[0].Text[0]")}
+        ></Text>
+      </View>
+    </Flex>
+  );
+}
---
+++ src/ui-components/index.js
@@ -0,0 +1,17 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+export { default as CardE } from "./CardE";
+export { default as HeroLayout2 } from "./HeroLayout2";
+export { default as Features4x1 } from "./Features4x1";
+export { default as CardD } from "./CardD";
+export { default as FAQItem } from "./FAQItem";
+export { default as CardA } from "./CardA";
+export { default as Post } from "./Post";
+export { default as SocialA } from "./SocialA";
+export { default as CardB } from "./CardB";
+export { default as Features2x2 } from "./Features2x2";
+export { default as SocialB } from "./SocialB";
